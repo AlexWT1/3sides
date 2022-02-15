a31cdf94b5828d7b68b48b8a9d77415d822f0fc1
Alex <Alex@DESKTOP-PIJTJQ7>
2022-02-15 20:02:06 +0300

    Добавьте файлы проекта.

---
+++ 3sides/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3sides
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a, b, c, p, s;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Convert.ToDouble(textBox3.Text);
+            p = (a + b + c) / 2;
+            s = Math.Round(Math.Sqrt(p*(p-a)*(p-b)*(p-c)),1);
+
+            MessageBox.Show("Площадь треугольника = " + s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
